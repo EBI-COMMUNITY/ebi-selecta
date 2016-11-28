430d95c126b431afe9a8038a91f9c7ff2493a953
nimpak <nim.pak@gmail.com>
2016-11-28 03:22:31 +0000

    .

---
+++ scripts/selectadb.py
@@ -0,0 +1,15 @@
+
+
+class selection:
+
+    def __init__(self, selection_id, datahub,tax_id,study_accession,run_accession,pipeline_name,analysis_id,public,analyst_webin_id):
+        self.selection_id = selection_id
+        self.datahub = datahub
+        self.tax_id = tax_id
+        self.study_accession=study_accession
+        self.run_accession=run_accession
+        self.pipeline_name=pipeline_name
+        self.analysis_id=analysis_id
+        self.public=public
+        self.analyst_webin_id=analyst_webin_id
+
