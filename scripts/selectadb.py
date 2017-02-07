7313824e3b43e7ba9f1636392ef7b75f280fdf85
nimpak <nim.pak@gmail.com>
2017-02-07 18:14:51 +0000

    added new properties

--- scripts/selectadb.py
+++ scripts/selectadb.py
@@ -30,6 +30,11 @@ class properties:
 		dbhost_provided=False
 		dbname_provided=False
 		dtu_cge_databases_provided=False
+        emc_slim_program_provided=False
+        emc_slim_property_file_provided=False
+        analysis_submission_mode_provided=False
+        analysis_submission_action_provided=False
+        analysis_submission_url_provided=False
 	        
 		
 		for l in lines:
@@ -55,9 +60,24 @@ class properties:
 			elif pair[0].lower()=='dbname':
 				self.dbname=pair[1]
 				dbname_provided=True
+            elif pair[0].lower()=='emc_slim_program':
+                self.emc_slim_program=pair[1]
+                emc_slim_program_provided=True
+            elif pair[0].lower()=='emc_slim_property_file':
+                self.emc_slim_property_file=pair[1]
+                emc_slim_property_file_provided=True
 			elif pair[0].lower()=='dtu_cge_databases':
 				self.dtu_cge_databases=pair[1]
 				dtu_cge_databases_provided=True
+            elif pair[0].lower()=='analysis_submission_mode':
+				self.analysis_submission_mode=pair[1]
+				analysis_submission_mode_provided=True
+            elif pair[0].lower()=='analysis_submission_action':
+                self.analysis_submission_action=pair[1]
+                analysis_submission_action_provided=True
+            elif pair[0].lower()=='dtu_cge_databases':
+                self.analysis_submission_url=pair[1]
+                analysis_submission_url_provided=True
 				
 		
 		if workdir_provided==False:
@@ -74,10 +94,20 @@ class properties:
 		   self.dbhost=''
 		if dbname_provided==False:
 		   self.dbname=''
-		if dtu_cge_databases_provided==False:
-		   self.dtu_cge_databases=''
-		  
-		
+        if emc_slim_program_provided==False:
+           self.emc_slim_program=''
+        if emc_slim_property_file_provided==False:
+           self.emc_slim_property_file=''
+		if analysis_submission_mode_provided==False:
+		   self.analysis_submission_mode=''
+        if dtu_cge_databases_provided==False:
+           self.dtu_cge_databases=''
+        if analysis_submission_action_provided==False:
+           self.analysis_submission_action=''
+        if analysis_submission_url_provided==False:
+           self.analysis_submission_url=''
+
+
 		
 		
 		
