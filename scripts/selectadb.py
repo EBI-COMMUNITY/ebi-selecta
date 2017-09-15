761924a8c8894c041bd8f507cf99a64fa4b73d17
alakob <francoisalako@yahoo.co.uk>
2017-09-15 11:02:16 +0100

    1) Datahub metadata are now retrieved from the pathogen portal 2) selection_to_attribute.py, selectadb.py and pipelineAttribites.py are ported to python3

--- scripts/selectadb.py
+++ scripts/selectadb.py
@@ -1,116 +1,116 @@
+from mock.mock import self
 
 
 class selection:
+    def __init__(self, selection_id, datahub, tax_id, study_accession, run_accession, pipeline_name, analysis_id,
+                 public, analyst_webin_id):
+        self.selection_id = selection_id
+        self.datahub = datahub
+        self.tax_id = tax_id
+        self.study_accession = study_accession
+        self.run_accession = run_accession
+        self.pipeline_name = pipeline_name
+        self.analysis_id = analysis_id
+        self.public = public
+        self.analyst_webin_id = analyst_webin_id
 
-	def __init__(self, selection_id, datahub,tax_id,study_accession,run_accession,pipeline_name,analysis_id,public,analyst_webin_id):
-		self.selection_id = selection_id
-		self.datahub = datahub
-		self.tax_id = tax_id
-		self.study_accession=study_accession
-		self.run_accession=run_accession
-		self.pipeline_name=pipeline_name
-		self.analysis_id=analysis_id
-		self.public=public
-		self.analyst_webin_id=analyst_webin_id
-		
-		
-		
-		
+
 class properties:
-	
-	def __init__(self, property_file):
-		with open(property_file) as f:
-			 lines = f.readlines()
-		
-		workdir_provided=False
-		workdir_input_provided=False
-		archivedir_provided=False
-		dbuser_provided=False
-		dbpassword_provided=False
-		dbhost_provided=False
-		dbname_provided=False
-		dtu_cge_databases_provided=False
-		emc_slim_program_provided=False
-		emc_slim_property_file_provided=False
-		analysis_submission_mode_provided=False
-		analysis_submission_action_provided=False
-		analysis_submission_url_provided=False
-                max_core_job_provided=False
-			
-		
-		for l in lines:
-			pair=l.strip().split(":")
-			if pair[0].lower()=='workdir':
-				self.workdir=pair[1]
-				workdir_provided=True
-                        elif pair[0].lower()=='max_core_job':
-                                self.max_core_job=pair[1]
-                                max_core_job_provided=True
-			elif pair[0].lower()=='workdir_input':
-				self.workdir_input=pair[1]
-				workdir_input_provided=True
-			elif pair[0].lower()=='archivedir':
-				self.archivedir=pair[1]
-				archivedir_provided=True
-			elif pair[0].lower()=='dbuser':
-				self.dbuser=pair[1]
-				dbuser_provided=True
-			elif pair[0].lower()=='dbpassword':
-				self.dbpassword=pair[1]
-				dbpassword_provided=True
-			elif pair[0].lower()=='dbhost':
-				self.dbhost=pair[1]
-				dbhost_provided=True
-			elif pair[0].lower()=='dbname':
-				self.dbname=pair[1]
-				dbname_provided=True
-			elif pair[0].lower()=='emc_slim_program':
-				self.emc_slim_program=pair[1]
-				emc_slim_program_provided=True
-			elif pair[0].lower()=='emc_slim_property_file':
-				self.emc_slim_property_file=pair[1]
-				emc_slim_property_file_provided=True
-			elif pair[0].lower()=='dtu_cge_databases':
-				self.dtu_cge_databases=pair[1]
-				dtu_cge_databases_provided=True
-			elif pair[0].lower()=='analysis_submission_mode':
-				self.analysis_submission_mode=pair[1]
-				analysis_submission_mode_provided=True
-			elif pair[0].lower()=='analysis_submission_action':
-				self.analysis_submission_action=pair[1]
-				analysis_submission_action_provided=True
-			elif pair[0].lower()=='dtu_cge_databases':
-				self.analysis_submission_url=pair[1]
-				analysis_submission_url_provided=True
-				
-		
-		if workdir_provided==False:
-			self.workdir=''
-                if max_core_job_provided==False:
-                        self.max_core_job=10
-		if workdir_input_provided==False:
-			self.workdir_input=''
-		if archivedir_provided==False:
-		   self.archivedir=''
-		if dbuser_provided==False:
-		   self.dbuser_provided=''
-		if dbpassword_provided==False:
-		   self.dbpassword_provided=''
-		if dbhost_provided==False:
-		   self.dbhost=''
-		if dbname_provided==False:
-		   self.dbname=''
-		if emc_slim_program_provided==False:
-		   self.emc_slim_program=''
-		if emc_slim_property_file_provided==False:
-		   self.emc_slim_property_file=''
-		if analysis_submission_mode_provided==False:
-		   self.analysis_submission_mode=''
-		if dtu_cge_databases_provided==False:
-		   self.dtu_cge_databases=''
-		if analysis_submission_action_provided==False:
-		   self.analysis_submission_action=''
-		if analysis_submission_url_provided==False:
-		   self.analysis_submission_url=''
+    def __init__(self, property_file):
+        with open(property_file) as f:
+            lines = f.readlines()
+            print('|-|' * 50)
+            print(type(lines))
+            print('|-|' * 50)
 
+            workdir_provided = False
+            workdir_input_provided = False
+            archivedir_provided = False
+            dbuser_provided = False
+            dbpassword_provided = False
+            dbhost_provided = False
+            dbname_provided = False
+            dtu_cge_databases_provided = False
+            emc_slim_program_provided = False
+            emc_slim_property_file_provided = False
+            analysis_submission_mode_provided = False
+            analysis_submission_action_provided = False
+            analysis_submission_url_provided = False
+            max_core_job_provided = False
 
+            for l in lines:
+                print("*" * 100)
+                print(l)
+                print("*" * 100)
+                pair = l.strip().split(":")
+                if pair[0].lower() == 'workdir':
+                    self.workdir = pair[1]
+                    workdir_provided = True
+                elif pair[0].lower() == 'max_core_job':
+                    self.max_core_job = pair[1]
+                    max_core_job_provided = True
+                elif pair[0].lower() == 'workdir_input':
+                    self.workdir_input = pair[1]
+                    workdir_input_provided = True
+                elif pair[0].lower() == 'archivedir':
+                    self.archivedir = pair[1]
+                    archivedir_provided = True
+                elif pair[0].lower() == 'dbuser':
+                    self.dbuser = pair[1]
+                    dbuser_provided = True
+                elif pair[0].lower() == 'dbpassword':
+                    self.dbpassword = pair[1]
+                    dbpassword_provided = True
+                elif pair[0].lower() == 'dbhost':
+                    self.dbhost = pair[1]
+                    dbhost_provided = True
+                elif pair[0].lower() == 'dbname':
+                    self.dbname = pair[1]
+                    dbname_provided = True
+                elif pair[0].lower() == 'emc_slim_program':
+                    self.emc_slim_program = pair[1]
+                    emc_slim_program_provided = True
+                elif pair[0].lower() == 'emc_slim_property_file':
+                    self.emc_slim_property_file = pair[1]
+                    emc_slim_property_file_provided = True
+                elif pair[0].lower() == 'dtu_cge_databases':
+                    self.dtu_cge_databases = pair[1]
+                    dtu_cge_databases_provided = True
+                elif pair[0].lower() == 'analysis_submission_mode':
+                    self.analysis_submission_mode = pair[1]
+                    analysis_submission_mode_provided = True
+                elif pair[0].lower() == 'analysis_submission_action':
+                    self.analysis_submission_action = pair[1]
+                    analysis_submission_action_provided = True
+                elif pair[0].lower() == 'dtu_cge_databases':
+                    self.analysis_submission_url = pair[1]
+                    analysis_submission_url_provided = True
+
+            if not workdir_provided:
+                self.workdir = ''
+            if not max_core_job_provided:
+                self.max_core_job = 10
+            if not workdir_input_provided:
+                self.workdir_input = ''
+            if not archivedir_provided:
+                self.archivedir = ''
+            if not dbuser_provided:
+                self.dbuser_provided = ''
+            if not dbpassword_provided:
+                self.dbpassword_provided = ''
+            if not dbhost_provided:
+                self.dbhost = ''
+            if not dbname_provided:
+                self.dbname = ''
+            if not emc_slim_program_provided:
+                self.emc_slim_program = ''
+            if not emc_slim_property_file_provided:
+                self.emc_slim_property_file = ''
+            if not analysis_submission_mode_provided:
+                self.analysis_submission_mode = ''
+            if not dtu_cge_databases_provided:
+                self.dtu_cge_databases = ''
+            if not analysis_submission_action_provided:
+                self.analysis_submission_action = ''
+            if not analysis_submission_url_provided:
+                self.analysis_submission_url = ''
